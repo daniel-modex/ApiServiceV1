7314299eb4aba1708eb94775e85bcc3c9e3b1634
daniel-modex <danielmodex33@gmail.com>
2025-04-17 22:30:35 +0530

    Add project files.

---
+++ AppApiService/Controllers/Registeration/PatientRegisterController.cs
@@ -0,0 +1,19 @@
+﻿using AppServices.DAL.RegisterationDAL;
+using AppServices.Models.Patient;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppApiService.Controllers.Registeration
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class PatientRegisterController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Register")]
+        public ActionResult<object> RegisterPatient(RegisterPatient Data)
+        {
+            return Ok(PatientRegisteration.NewPatientRegister(Data));
+        }
+    }
+}
